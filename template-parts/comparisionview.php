e0f9e728cacc3448929341e810e98c6386c7cf12
Eunice Dhivya <divi.seed@gmail.com>
2019-07-19 19:19:12 +0530

    Buttons: Add disabled state to comparision view buttons

--- template-parts/comparisionview.php
+++ template-parts/comparisionview.php
@@ -1,7 +1,7 @@
 <h2>Comparison: 2014 and 2019</h2>
 <div class="comparenav hideindesktop">
-        <button id="show2014" class="electionbutton">2014</button>
-        <button id="show2019" class="active electionbutton">2019</button>
+        <button id="show2014" class="electionbutton" >2014</button>
+        <button id="show2019" class="electionbutton" disabled="disabled">2019</button>
 </div>
 <div class="pull-left const-map-2014 hideinmobile" >
     <img src="noimages/2014.jpg" alt="">  
